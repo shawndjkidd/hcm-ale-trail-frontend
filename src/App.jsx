b21e91d22f9ee9d7ef1c3e6d227c13486a48e2a6
Shawn Kidd <shawndjkidd@gmail.com>
2026-03-04 12:09:45 +0700

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -420,7 +420,7 @@ export default function App() {
   }
 
   return (
-    <div className="app">
+    <div className="app" data-lang={language}>
       {showWelcome && (
         <WelcomeModal language={language} setLanguage={setLanguage} onComplete={handleUserRegistration} onSignIn={() => setShowAuth(true)} />
       )}
